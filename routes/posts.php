0a020584b7dc56ff571ad46f544ed7d1a7f2a4ae
Lucas Gabriel <lucas1020111940@hotmail.com>
2026-03-27 08:49:12 -0300

    refactor: post route

--- routes/posts.php
+++ routes/posts.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Post\{PostController, PostPublicController};
 
-Route::get('portal/posts', [PostPublicController::class, 'index']);
+Route::prefix('portal')->group(function () {
+    Route::get('/posts', [PostPublicController::class, 'index']);
+});
 
-Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
+Route::middleware('auth:sanctum')->prefix('editor')->group(function () {
     Route::apiResource('posts', PostController::class);
     Route::patch('posts/{post}/toggle-publish', [PostController::class, 'toggleStatus']);
 });
