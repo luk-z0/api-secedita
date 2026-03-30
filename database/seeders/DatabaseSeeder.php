b8fac97a5615baa5505c66249b07ead3e903db88
Lucas Gabriel <lucas1020111940@hotmail.com>
2026-03-30 10:40:42 -0300

    feat: deploy config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            RoleSeeder::class,
-        ]);
+        $this->call(RoleSeeder::class);
+
         $roles = \App\Models\Role::all();
 
         $admin = User::factory()->create([
